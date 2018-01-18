be61bc874492ead18d21793118d48f322fc61db1
juwon24luv123 <juwon24luv1@yahoo.com>
2018-01-18 01:34:04 -0800

    setup heroku

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -16,139 +16,62 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     console.log('connected to MongoDB server');
 
 
-    // db.collection('User').insertMany([
-    //     {name: 'idowu',
-    //         age: 24,
-    //         occupation: 'english teacher',
-    //         location: 'Akure'},
+    db.collection('User').insertMany([
+        {name: 'idowu',
+            age: 24,
+            occupation: 'english teacher',
+            location: 'Akure'},
 
-    //         {name: 'ayo',
-    //         age: 24,
-    //         occupation: 'english teacher',
-    //         location: 'Akure'},
+            {name: 'ayo',
+            age: 24,
+            occupation: 'english teacher',
+            location: 'Akure'},
 
-    //         {name: 'nike',
-    //         age: 24,
-    //         occupation: 'english teacher',
-    //         location: 'Akure'},
+            {name: 'nike',
+            age: 24,
+            occupation: 'english teacher',
+            location: 'Akure'},
 
-    //         {name: 'bayo',
-    //         age: 24,
-    //         occupation: 'english teacher',
-    //         location: 'Akure'},
+            {name: 'bayo',
+            age: 24,
+            occupation: 'english teacher',
+            location: 'Akure'},
 
-    //         {name: 'bola',
-    //         age: 24,
-    //         occupation: 'english teacher',
-    //         location: 'Akure'}
-    // ], (err,result) => {
-    //         if(err) {
-    //             return console.log('Unable to insert user', err);
-    //         }
-    //         console.log(result.ops)
-    //     });
-        
-    
+            {name: 'bola',
+            age: 24,
+            occupation: 'english teacher',
+            location: 'Akure'},
+
+               {name: 'bola',
+            age: 24,
+            occupation: 'english teacher',
+            location: 'Akure'}
+    ], (err,result) => {
+            if(err) {
+                return console.log('Unable to insert user', err);
+            }
+            console.log(result.ops)
+        });
 
 
 
+//     var User = argv.command;
+//     console.log('command');
+   
+//    console.log('Command: ', User);
+   
+//     if (User === 'add') {
+//         console.log('Adding new note');
+//     } else if (User === 'list') {
+//         console.log('Listing all notes');
+//     } else if (User === 'read') {
+//         console.log('reading note');
+//     } else if (User === 'remove') {
+//         console.log('Removing note');
+//     } else {
+//         console.log('User not recognized');
+//     }
 
-
-
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err,result) =>{
-    //    if(err) {
-    //      return console.log('Unable to insert todo', err);
-    //    }
-    //    console.log(JSON.stringify(result.ops, undefined, 2))
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'Bankole',
-    //     age: 24,
-    //     occupation: 'english teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops)
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'ola',
-    //     age: 25,
-    //     occupation: 'math teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops)
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'shade',
-    //     age: 30,
-    //     occupation: 'french teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops)
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'yink',
-    //     age: 40,
-    //     occupation: 'yoruba teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops)
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'pelumi',
-    //     age: 45,
-    //     occupation: 'arab teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops)
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'taju',
-    //     age: 25,
-    //     occupation: 'science teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops)
-    // });
-
-    // db.collection('User').insertOne({
-    //     name: 'ahmed',
-    //     age: 35,
-    //     sex: 'male',
-    //     occupation: 'science teacher',
-    //     location: 'Akure'
-    // }, (err,result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(result.ops);
-    // });
 
     db.close();
 });
