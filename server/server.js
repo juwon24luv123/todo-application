790d3388c8239a504f08fe6bf9cde5b6f5f00c72
juwon24luv123 <juwon24luv1@yahoo.com>
2018-01-25 02:35:40 -0800

    Add get /User/ me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -108,8 +109,6 @@ app.post('/users', (req, res) =>{
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
-
-
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
@@ -117,8 +116,14 @@ app.post('/users', (req, res) =>{
     }).catch((e) => {
         res.status(400).send(e); 
     })
-
 });
+
+// PRIVATE ROUTE
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
 
 
 app.listen(port,() => {
